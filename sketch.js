8eaa275a106d55798edc5eaa94363cf3bdc17f20
MangoNotThere <117975356+MangoNotThere@users.noreply.github.com>
2022-12-27 18:43:06 -0800

    makes it look cooler. Also another patch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var vLine=["Vecna strikes", "Vecna attacks", "Upside-down consumes", "Vecna takes", "Upside-down takes", "Vecna sinks", "Vecna kablooied", "The Upside-down engulfs","The Upside-down consumes","The Upside-down strikes"]
 var places=["Wheeler House", "Henderson House", "Trailer Park", "Hawkins High","Video Store","Byers' House", "Lenora Hills High", "Rink-O-Mania", "Surfer Boy Pizza", "Police Station", "County Lane Cafe", "Pennhurst Assylum", "Suzie's House", "Silo Lab", "Yuri's Fish N Fly", "Kamchatka Church", "Russian Prison", "Counselor Kelly's House", "Hawkin's Cemetery", "Lovers' Lake","Hawkins Lab","Creel House"]
 var vplace
+let cvline
 var lc1=255
 var lc2=255
 var lc3=255
@@ -8,32 +9,25 @@ let d61
 let d62
 //double count
 let dc=0
+//scroll x
+let sx
+let dtext
+let vtext
 function setup() {
   
   createCanvas(windowWidth, windowHeight);
   background(0)
 }
 
-function draw() {
-  noStroke()
-  fill(255)
-  textSize(windowHeight/20)
-  text("Spin Clock",windowWidth/8,windowHeight/8)
-  text("Roll Dice", windowWidth/8*5, windowHeight/8)
-  strokeWeight(4)
-  stroke(255)
-  line(windowWidth/2,0,windowWidth/2,windowHeight/5*4-4)
-  stroke(lc1,lc2,lc3)
-  line(0,windowHeight/5*4,windowWidth,windowHeight/5*4)
 
-  if(mouseIsPressed){
-  mouseClicked()
-}
-}
+
+
 function keyPressed(){
- 
+ sx=0
+  
   if(key==='f'){
-     background(0)
+    dtext=0 
+    
     strokeWeight(4)
     stroke(179, 18, 18)
     fill(0)
@@ -41,32 +35,25 @@ function keyPressed(){
     fill(255)
     textSize(windowHeight/25)
     noStroke()
+    cvline=random(0,vLine.length)
     vplace=floor(random(1,22))
-    text(vLine[floor(random(0, vLine.length))]+" "+vplace+", "+places[vplace-1]+"!",5,windowHeight/5*4+5,windowWidth-5,windowHeight)
+  vtext=1
+    lc1=179
+    lc2=18
+    lc3=18
+    
   }
-
 if(key==='j'){
-      background(0)
+    vtext=0
+    background(0)
     strokeWeight(4)
     stroke(45, 65, 240)
     fill(0)
     rect(2,windowHeight/5*4,windowWidth-4,windowHeight-4)
     fill(255)
-    textSize(windowHeight/25)
-    noStroke()
-      d61=floor(random(1,6))
-      d62 = floor(random(1,6))
-      if(d61!==d62){
-    text("You roll "+d61+" and "+d62+"! Move "+ (d61+d62)+" spaces!",5,windowHeight/5*4+5,windowWidth-5,windowHeight)
-        dc=0
-      }else{
-        if(dc===3){
-          text("Uh-oh!You roll a "+d61+" and a "+d62+"! Go to jail. Do not pass go. Do not collect $200.",5,windowHeight/5*4+5,windowWidth-5,windowHeight)
-        }else{
-        text("You roll "+d61+" and "+d62+". Doubles! Move "+ (d61+d62)+" spaces!",5,windowHeight/5*4+5,windowWidth-5,windowHeight)
-        dc=dc+1
-        }
-        }
+    dtext=1
+    d61=floor(random(1,6))
+    d62 = floor(random(1,6))
     lc1=45
     lc2=65
     lc3=240
@@ -75,8 +62,9 @@ if(key==='j'){
 }
 
 function mouseClicked(){
+  sx=0
   if(mouseX<windowWidth/2){
-    background(0)
+    dtext=0 
     strokeWeight(4)
     stroke(179, 18, 18)
     fill(0)
@@ -84,23 +72,71 @@ function mouseClicked(){
     fill(255)
     textSize(windowHeight/25)
     noStroke()
+    cvline=random(0,vLine.length)
     vplace=floor(random(1,22))
-    text(vLine[floor(random(0, vLine.length))]+" "+vplace+", "+places[vplace-1]+"!",5,windowHeight/5*4+5,windowWidth-5,windowHeight)
+  vtext=1
     lc1=179
     lc2=18
     lc3=18
   }
     if(mouseX>windowWidth/2){
-      background(0)
+     vtext=0
+    background(0)
     strokeWeight(4)
     stroke(45, 65, 240)
     fill(0)
     rect(2,windowHeight/5*4,windowWidth-4,windowHeight-4)
     fill(255)
+    d61=floor(random(1,6))
+    d62 = floor(random(1,6))
+    dtext=1
+    lc1=45
+    lc2=65
+    lc3=240
+    
+  }
+}
+function draw() {
+  background(0)
+  noStroke()
+  fill(255)
+  textSize(windowHeight/20)
+  text("Spin Clock",windowWidth/8,windowHeight/8)
+  text("Roll Dice", windowWidth/8*5, windowHeight/8)
+  noFill()
+  strokeWeight(4)
+  stroke(255)
+  line(windowWidth/2,0,windowWidth/2,windowHeight/5*4-4)
+  stroke(lc1,lc2,lc3)
+  line(0,windowHeight/5*4,windowWidth,windowHeight/5*4)
+  
+  if(mouseIsPressed){
+  mouseClicked()
+}
+  noStroke()
+  fill(0)
+  rect(sx+4,windowHeight/5*4+4,windowWidth-4)
+if(vtext===1){
+    
+    strokeWeight(4)
+    stroke(179, 18, 18)
+    fill(0)
+    rect(2,windowHeight/5*4,windowWidth-4,windowHeight-4)
+    fill(255)
     textSize(windowHeight/25)
     noStroke()
-      d61=floor(random(1,6))
-      d62 = floor(random(1,6))
+
+    text(vLine[floor(cvline)]+" "+vplace+", "+places[vplace-1]+"!",5,windowHeight/5*4+5,windowWidth-5,windowHeight)
+}
+if(dtext===1){
+  strokeWeight(4)
+    stroke(45, 65, 240)
+    fill(0)
+    rect(2,windowHeight/5*4,windowWidth-4,windowHeight-4)
+    fill(255)
+  fill(255)
+  noStroke()
+  textSize(windowHeight/25)
       if(d61!==d62){
     text("You roll "+d61+" and "+d62+"! Move "+ (d61+d62)+" spaces!",5,windowHeight/5*4+5,windowWidth-5,windowHeight)
         dc=0
@@ -112,9 +148,11 @@ function mouseClicked(){
         dc=dc+1
         }
         }
-    lc1=45
-    lc2=65
-    lc3=240
-    
-  }
+}
+  
+  if(sx<windowWidth-12){
+  sx=sx+windowWidth/200;
+}
+  fill(0)
+  rect(sx+4,windowHeight/5*4+2,windowWidth-(sx+8),windowHeight/5)
 }
